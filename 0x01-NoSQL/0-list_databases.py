0580eda662e159cfb88331add527f0b555d91403
“sitoaustin” <onyedibesixtusna@gmail.com>
2023-10-17 15:23:55 +0100

    Show all dbs

--- 0x01-NoSQL/0-list_databases.py
+++ 0x01-NoSQL/0-list_databases.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-from pymongo import MongoClient
+// show all the databases
 
-uri = """mongodb://127.0.0.1:27017/?compressors
-            =disabled&gssapiServiceName=mongodb"""
-client = MongoClient(uri)
-
-dbs = client.list_database_names()
-
-print(dbs)
+show dbs
